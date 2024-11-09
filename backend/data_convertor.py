51646051f5b4bcfad8587a8760c1b2f3336408a3
lisawasdf <lisa.wilplinger@icloud.com>
2024-11-09 12:17:17 +0100

    backend einrichten

---
+++ backend/data_convertor.py
@@ -0,0 +1,14 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+import pandas as pd
+
+data_path = "datasource/amd_amlage_jaenner.csv"
+df = pd.read_csv(data_path)
+
+documents = df.apply(lambda row: " ".join(map(str, row.values)), axis=1).tolist()
+
+# hier das embedding von geminini einbinden, mit api-schlüssel
+embedding = ()
+
+vector_store = Chroma.from_texts(documents, embedding, persist_directory="data/chroma_index")
+vector_store.persist()
